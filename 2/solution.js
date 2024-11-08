186bd8bcff036cb748496ce1e9b2ec3e9c46c04f
CheLaLuna <160347343+CheLaLuna@users.noreply.github.com>
2024-11-08 18:08:40 +0300

    Update solution.js

--- 2/solution.js
+++ 2/solution.js
@@ -8,5 +8,21 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+const sql = postgres(config);
 
+export default async function solution(articles) {
+  const ids = [];
+
+  for (const article of articles) {
+    const { title, description } = article;
+    const result = await sql`
+      INSERT INTO articles (title, description)
+      VALUES (${title}, ${description})
+      RETURNING id
+    `;
+    ids.push(result[0].id);
+  }
+
+  return ids;
+}
 // END
